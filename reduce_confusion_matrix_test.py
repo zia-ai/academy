80e90f1f49b5f2317dc67e82a75c85aa85dd16bd
Mohammed Fayaz Ansar Jelani <fayaz-jelani@thinjetty.com>
2023-03-28 09:34:23 +0100

    separation of treemap tiles using parent-child heirarchy

--- reduce_confusion_matrix_test.py
+++ reduce_confusion_matrix_test.py
@@ -29,16 +29,74 @@ def check_reduce_confusion_matrix(confusion_matrix: numpy.matrix, labels: list, 
         assert(isinstance(output_reduced_matrix,pandas.DataFrame))
         assert(output_reduced_matrix.equals(actual_reduced_matrix))
 
-def test_find_top_intent_pair():
-    reduced_matrix = pandas.read_csv('./examples/reduced_confusion_matrix_4.csv',index_col=0)
-    total_mispredictions = 2583
-    output_sorted_pair = reduce_confusion_matrix.find_top_intent_pair(reduced_matrix,total_mispredictions)
-    print(output_sorted_pair)
-    actual_sorted_pair = [('alarm_query <-> audio_volume_mute', [70, 2.71]), ('audio_volume_up <-> alarm_query', [60, 2.32]), 
-                          ('alarm_set <-> audio_volume_up', [40, 1.55]), ('audio_volume_mute <-> audio_volume_down', [30, 1.16]), 
-                          ('audio_volume_up <-> audio_volume_mute', [11, 0.43]), ('audio_volume_up <-> audio_volume_down', [11, 0.43]), 
-                          ('alarm_set <-> alarm_query', [5, 0.19]), ('audio_volume_up <-> audio_volume_up', [0, 0.0]), 
-                          ('alarm_query <-> alarm_query', [0, 0.0]), ('alarm_query <-> audio_volume_down', [0, 0.0]), 
-                          ('alarm_set <-> audio_volume_mute', [0, 0.0]), ('alarm_set <-> audio_volume_down', [0, 0.0]), 
+def test_rcm_m_2():
+    df_matrix = pandas.read_csv('./examples/confusion_matrix.csv',index_col=0)
+    labels=list(df_matrix.index)
+    confusion_matrix = df_matrix.to_numpy()
+    top_misprediction = 2
+    reduced_cm_df = reduce_confusion_matrix.reduce_confusion_matrix(confusion_matrix,labels,top_misprediction)
+    assert(reduced_cm_df.loc["alarm_query","alarm_query"] == 0)
+    assert(reduced_cm_df.loc["alarm_query","audio_volume_mute"] == 70)
+    assert(reduced_cm_df.loc["alarm_query","Total"] == 70)
+    assert(reduced_cm_df.loc["audio_volume_up","alarm_query"] == 60)
+    assert(reduced_cm_df.loc["audio_volume_up","audio_volume_mute"] == 7)
+    assert(reduced_cm_df.loc["audio_volume_up","Total"] == 67)
+    assert(reduced_cm_df.loc["Total","alarm_query"] == 60)
+    assert(reduced_cm_df.loc["Total","audio_volume_mute"] == 77)
+    assert(reduced_cm_df.loc["Total","Total"] == 137)
+    assert(reduced_cm_df.shape == (3,3))
+
+    total_mispredictions = reduce_confusion_matrix.calc_total_mispredictions(confusion_matrix)
+    assert(total_mispredictions == 360)
+
+    actual_sorted_pair = [("alarm_query <-> audio_volume_mute",[70,19.44]),
+                          ("audio_volume_up <-> alarm_query",[60,16.67]),
+                          ("audio_volume_up <-> audio_volume_mute",[7,1.94]),
+                          ("alarm_query <-> alarm_query",[0,0.0])]
+    
+    output_sorted_pair = reduce_confusion_matrix.find_top_intent_pair(reduced_cm_df,total_mispredictions)
+    assert(actual_sorted_pair == output_sorted_pair)
+
+
+def test_rcm_m_4():
+    df_matrix = pandas.read_csv('./examples/confusion_matrix.csv',index_col=0)
+    labels=list(df_matrix.index)
+    confusion_matrix = df_matrix.to_numpy()
+    top_misprediction = 4
+    reduced_cm_df = reduce_confusion_matrix.reduce_confusion_matrix(confusion_matrix,labels,top_misprediction)
+    assert(reduced_cm_df.shape == (5,5))
+    assert(list(reduced_cm_df.index) == ["audio_volume_up", "alarm_query","alarm_set","audio_volume_mute","Total"])
+    assert(list(reduced_cm_df.columns) == ["audio_volume_mute","alarm_query","audio_volume_up","audio_volume_down","Total"])
+    assert(reduced_cm_df.loc["alarm_query","alarm_query"] == 0)
+    assert(reduced_cm_df.loc["alarm_query","audio_volume_mute"] == 70)
+    assert(reduced_cm_df.loc["audio_volume_up","alarm_query"] == 60)
+    assert(reduced_cm_df.loc["audio_volume_up","audio_volume_mute"] == 7)
+    assert(reduced_cm_df.loc["audio_volume_mute","audio_volume_down"] == 30)
+    assert(reduced_cm_df.loc["Total","Total"] == 227)
+
+
+    total_mispredictions = reduce_confusion_matrix.calc_total_mispredictions(confusion_matrix)
+    assert(total_mispredictions == 360)
+
+    actual_sorted_pair = [('alarm_query <-> audio_volume_mute', [70, 19.44]), 
+                          ('audio_volume_up <-> alarm_query', [60, 16.67]), 
+                          ('alarm_set <-> audio_volume_up', [40, 11.11]), 
+                          ('audio_volume_mute <-> audio_volume_down', [30, 8.33]), 
+                          ('audio_volume_up <-> audio_volume_mute', [11, 3.06]), 
+                          ('audio_volume_up <-> audio_volume_down', [11, 3.06]), 
+                          ('alarm_set <-> alarm_query', [5, 1.39]), 
+                          ('audio_volume_up <-> audio_volume_up', [0, 0.0]), 
+                          ('alarm_query <-> alarm_query', [0, 0.0]), 
+                          ('alarm_query <-> audio_volume_down', [0, 0.0]), 
+                          ('alarm_set <-> audio_volume_mute', [0, 0.0]), 
+                          ('alarm_set <-> audio_volume_down', [0, 0.0]), 
                           ('audio_volume_mute <-> audio_volume_mute', [0, 0.0])]
-    assert(output_sorted_pair == actual_sorted_pair)
+    
+    output_sorted_pair = reduce_confusion_matrix.find_top_intent_pair(reduced_cm_df,total_mispredictions)
+
+    # case where two intersections having interchangable row and column names
+    assert(actual_sorted_pair[4] == output_sorted_pair[4])
+
+    # case where row and column name appears only once
+    assert(actual_sorted_pair[6] == output_sorted_pair[6])
+    
