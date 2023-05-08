957fde7974b5b978e915f1316cfb3a8d6904bca8
sbroadhurst-hf <108879611+sbroadhurst-hf@users.noreply.github.com>
2023-05-08 14:26:48 +0100

    Example of testing latency of predict end point

---
+++ predict.sh
@@ -0,0 +1,11 @@
+curl -L -X POST 'https://api.humanfirst.ai/v1alpha1/nlu/predict/$NAMESPACE/$PLAYBOOK' \
+-H 'Authorization: Bearer $BEARER_TOKEN' \
+-H 'Content-Type: application/json; charset=utf-8' \
+-H 'Accept: application/json' \
+-w "@curl-format.txt" 
+--data-raw '{
+  "namespace": "$NAMESPACE",
+  "playbook_id": "$PLAYBOOK",
+  "input_utterance": "Hello Charlie how fast can you go?"
+  }
+}'
