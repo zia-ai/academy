be0a1b36f8a7d22ac9487167aa75bc360aa3aa09
Mohammed Fayaz Ansar Jelani <fayaz-jelani@thinjetty.com>
2023-06-26 12:20:46 +0100

    fine-tuning foundational model

---
+++ fine_tune/convert_json_to_jsonl.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python # pylint: disable=missing-module-docstring
+# -*- coding: utf-8 -*-
+# ******************************************************************************************************************120
+#
+# python convert_json_to_jsonl.py
+#
+# *********************************************************************************************************************
+
+# standard imports
+import json
+
+# 3rd party imports
+import click
+import jsonlines
+
+@click.command()
+@click.option("-i", "--input_file", type=str, required=True, help="input file path")
+def main(input_file: str) -> None:
+    """Main Function"""
+
+    with open(input_file, mode="r", encoding="utf8") as file_obj:
+        data = json.load(file_obj)
+
+    file_output = input_file.replace(".json",".jsonl")
+    with jsonlines.open(file_output, 'w') as writer:
+        for _, value in data.items():
+            writer.write_all(value)
+
+if __name__ == "__main__":
+    main() # pylint: disable=no-value-for-parameter
