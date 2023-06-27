6525066b57b9cb0c586547164d5c07cffe62a0fc
Stephen Broadhurst <stephen@humanfirst.ai>
2023-06-27 15:42:28 +0100

    Coverage script updated with escape page and debug options

--- coverage.py
+++ coverage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python  # pylint: disable=missing-module-docstring
 # -*- coding: utf-8 -*-
 # ***************************************************************************80
 #
@@ -8,11 +8,11 @@
 
 # standard imports
 import json
+import copy
 
 # third part imports
 import pandas
 import click
-import copy
 
 # custom imports
 import humanfirst
@@ -25,41 +25,52 @@ import humanfirst_apis
 @click.option('-n', '--namespace', type=str, required=True, help='HumanFirst namespace')
 @click.option('-b', '--playbook', type=str, required=True, help='HumanFirst playbook id')
 @click.option('-t', '--bearertoken', type=str, default='', help='Bearer token to authorise with')
-@click.option('-c', '--convsetsource', default='', help='Filter results by a certain conversationset')
-@click.option('-x', '--searchtext', default='', help='text to search for in conversations')
-@click.option('-s', '--startisodate', default='', help='Date range to extract conversations from')
-@click.option('-e', '--endisodate', default='', help='Date range to extract conversations from')
-def main(username: str, password: int, namespace: bool, playbook: str, bearertoken: str, convsetsource: str, searchtext: str, startisodate: str, endisodate: str):
+@click.option('-c', '--convsetsource', type=str, default='', help='Filter results by a certain conversationset')
+@click.option('-x', '--searchtext', type=str, default='', help='text to search for in conversations')
+@click.option('-s', '--startisodate', type=str, default='', help='Date range to extract conversations from')
+@click.option('-e', '--endisodate', type=str, default='', help='Date range to extract conversations from')
+@click.option('-q', '--quit_after_pages', type=int, default=0, help='Date range to extract conversations from')
+@click.option('-d', '--debug', is_flag=True, default=False, help='Debug')
+def main(username: str, password: int, namespace: bool, playbook: str, bearertoken: str,
+         convsetsource: str, searchtext: str, startisodate: str, endisodate: str, quit_after_pages: int, debug: bool):
+    '''Main function'''
     write_coverage_csv(username, password, namespace, playbook, bearertoken,
-            convsetsource, searchtext, startisodate, endisodate)
+                       convsetsource, searchtext, startisodate, endisodate,
+                       quit_after_pages=quit_after_pages, debug=debug)
 
 
-def write_coverage_csv(username: str, 
-            password: int, 
-            namespace: bool, 
-            playbook: str, 
-            bearertoken: str, 
-            convsetsource: str, 
-            searchtext: str, 
-            startisodate: str, 
-            endisodate: str,
-            output_path: str = './data',
-            separator: str = ',',
-            page_size: int = 50):
+def write_coverage_csv(username: str,
+                       password: int,
+                       namespace: bool,
+                       playbook: str,
+                       bearertoken: str,
+                       convsetsource: str,
+                       searchtext: str,
+                       startisodate: str,
+                       endisodate: str,
+                       output_path: str = './data',
+                       separator: str = ',',
+                       page_size: int = 50,
+                       quit_after_pages: int = 0,
+                       debug: bool = False):
     '''Download the full unlabelled model for the conversation set source with all the data science statistics
     inferred from the provided playbook then write a csv containing prediction data to the path provided with the
     separator provided'''
 
     headers = humanfirst_apis.process_auth(bearertoken, username, password)
     playbook_dict = humanfirst_apis.get_playbook(headers, namespace, playbook)
+
     df = get_conversationset_df(headers, namespace, playbook, convsetsource,
-                                searchtext, startisodate, endisodate, playbook_dict, page_size=page_size)
-    
+                                searchtext, startisodate, endisodate, playbook_dict,
+                                page_size=page_size, quit_after_pages=quit_after_pages, debug=debug)
+
     if not output_path.endswith('/'):
         output_path = output_path + '/'
 
-    df.to_csv(f'{output_path}{playbook_dict["name"]}', sep=separator, encoding="utf8", index=False)
+    output_uri = f'{output_path}{playbook_dict["name"]}'
+    df.to_csv(output_uri, sep=separator, encoding="utf8", index=False)
     print(df)
+
 
 def get_conversationset_df(
         headers: dict,
@@ -70,12 +81,14 @@ def get_conversationset_df(
         startisodate: str,
         endisodate: str,
         playbook_dict: dict,
-        page_size: int = 50) -> pandas.DataFrame:
+        page_size: int = 50,
+        quit_after_pages: int = 0,
+        debug: bool = False) -> pandas.DataFrame:
     '''Download the inferred statistics for the conversation set source for the provided
     playbook and return a data frame.  Pages through the very large data science data
     with each page of page_size'''
     labelled_workspace = humanfirst.HFWorkspace.from_json(playbook_dict)
-    assert (isinstance(labelled_workspace, humanfirst.HFWorkspace))
+    assert isinstance(labelled_workspace, humanfirst.HFWorkspace)
     intent_name_index = labelled_workspace.get_intent_index(delimiter="-")
     print("Got playbook and parsed it")
 
@@ -91,12 +104,16 @@ def get_conversationset_df(
         convsetsource=convsetsource,
         page_size=page_size
     )
-    results = extract_results(results, intent_name_index, response_json)
-    assert (isinstance(response_json, dict))
+    results = extract_results(
+        results, intent_name_index, response_json, debug=debug)
+    assert isinstance(response_json, dict)
     print(f'Page {i}: {len(results)}')
     i = i + 1
 
+    print(f"Quit after pages {quit_after_pages}")
     while "nextPageToken" in response_json:
+        if quit_after_pages > 0 and i >= quit_after_pages:
+            break
         response_json = humanfirst_apis.query_conversation_set(
             headers,
             namespace,
@@ -108,7 +125,7 @@ def get_conversationset_df(
             page_size=page_size,
             nextPageToken=response_json["nextPageToken"]
         )
-        assert (isinstance(response_json, dict))
+        assert isinstance(response_json, dict)
         if not "results" in response_json.keys() and "totalCount" in response_json.keys():
             print(f'totalCount: {response_json["totalCount"]}')
             break
@@ -121,7 +138,13 @@ def get_conversationset_df(
     return pandas.DataFrame(results)
 
 
-def extract_results(results: list, intent_name_index: dict, response_json):
+def dump_this(text: str, filename: str):
+    """Dump text to file in debug mode"""
+    with open(f"./data/{filename}.json", "w", encoding="utf8") as file_out:
+        file_out.write(json.dumps(text, indent=2))
+
+
+def extract_results(results: list, intent_name_index: dict, response_json, debug: bool = False):
     '''Extacts the desired data for the data frame from a set of results from the query end point in HumanFirst'''
     for result in response_json["results"]:
 
@@ -131,6 +154,9 @@ def extract_results(results: list, intent_name_index: dict, response_json):
         conv_obj["conv_created_at"] = result["annotatedConversation"]["conversation"]["createdAt"]
         conv_obj["conv_updated_at"] = result["annotatedConversation"]["conversation"]["updatedAt"]
 
+        if debug:
+            dump_this(result, conv_obj["convoid"])
+
         for i in range(len(result["annotatedConversation"]["conversation"]["inputs"])):
             try:
                 if "value" in result["annotatedConversation"]["conversation"]["inputs"][i].keys():
@@ -139,15 +165,15 @@ def extract_results(results: list, intent_name_index: dict, response_json):
                     conv_obj["utterance"] = ""
                 conv_obj["role"] = result["annotatedConversation"]["conversation"]["inputs"][i]["source"]
                 if "matches" in result["annotatedConversation"]["annotations"]["inputs_intents"]["inputs"][i].keys():
-                    conv_obj["intent_id"] = result["annotatedConversation"]["annotations"]["inputs_intents"]["inputs"][i]["matches"][0]["intentId"]
-                    conv_obj["score"] = result["annotatedConversation"]["annotations"]["inputs_intents"]["inputs"][i]["matches"][0]["score"]
+                    conv_obj["intent_id"] = result["annotatedConversation"]["annotations"]["inputs_intents"]["inputs"][i]["matches"][0]["intentId"] # pylint: disable=line-too-long
+                    conv_obj["score"] = result["annotatedConversation"]["annotations"]["inputs_intents"]["inputs"][i]["matches"][0]["score"] # pylint: disable=line-too-long
                     conv_obj["intent"] = intent_name_index[conv_obj["intent_id"]]
                 else:
-                    for f in ["intent_id", "score", "intent"]:
-                        conv_obj[f] = None
+                    for field in ["intent_id", "score", "intent"]:
+                        conv_obj[field] = None
                 conv_obj["seq"] = i
                 results.append(copy.deepcopy(conv_obj))
-            except Exception as e:
+            except Exception: # pylint: disable=broad-exception-caught
                 print("No idea what's up with this:")
                 print(json.dumps(result["annotatedConversation"]
                       ["conversation"]["inputs"][i], indent=2))
@@ -155,4 +181,4 @@ def extract_results(results: list, intent_name_index: dict, response_json):
 
 
 if __name__ == '__main__':
-    main()
+    main()  # pylint: disable=no-value-for-parameter
