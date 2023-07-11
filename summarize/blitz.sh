2944d3cfabb1ad499ea5482170a95bd7867a5799
Stephen Broadhurst <stephen@humanfirst.ai>
2023-07-11 12:31:33 +0100

    Diff and semantic drafts

--- summarize/blitz.sh
+++ summarize/blitz.sh
@@ -2,5 +2,5 @@
 SCRIPT_PATH=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 LOG_PATH=$SCRIPT_PATH/logs
 SUMMARIES_PATH=$SCRIPT_PATH/summaries
-find "${LOG_PATH}" -name "*.log" -delete
-find "${SUMMARIES_PATH}" -name "*.txt" -delete
+find "${LOG_PATH}" -maxdepth 1 -name "*.log" -delete 
+find "${SUMMARIES_PATH}" -maxdepth 1 -name "*.txt" -delete 
