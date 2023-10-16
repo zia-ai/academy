6fe0b395fd53597e2a37f51d1bcdea84d9756f27
Mohammed Fayaz Ansar Jelani <fayaz@humanfirst.ai>
2023-10-16 13:33:54 +0100

    tagging an intent with both its intent name and its parent intent name

--- summarize/10_turn.py
+++ summarize/10_turn.py
@@ -18,6 +18,7 @@ import datetime
 # 3rd party imports
 import click
 import pandas
+import numpy as np
 
 # Custom Imports
 import_path = os.path.dirname(os.path.realpath(__file__))
@@ -35,9 +36,14 @@ def main(jointo:str, spliton:str):
     with open(jointo,mode="r",encoding="utf8") as original_file:
         original_dict = json.load(original_file)
     df_original = pandas.json_normalize(original_dict["intents"])
-    all_names = df_original["name"].to_list()
+    df_original.set_index(["id"],drop=False,inplace=True)
+    df_original["tags"] = [[] for _ in range(len(df_original.index))]
+    df_original["metadata"] = [{} for _ in range(len(df_original.index))]
+    intent_id_dict = df_original[["name","tags","metadata"]].to_dict("index")
+
     tags = []
-    for name in all_names:
+    for id in intent_id_dict:
+        name = intent_id_dict[id]["name"]
         assert isinstance(name,str)
         potential_tags = name.split(spliton)
         if isinstance(potential_tags,str):
@@ -56,18 +62,19 @@ def main(jointo:str, spliton:str):
         tags.append(build_tags(tag_name))
     original_dict["tags"] = tags
 
+    for id in intent_id_dict:
+        for tag in tags:
+            if intent_id_dict[id]["name"].find(tag["name"]) >= 0:
+                intent_id_dict[id]["tags"].append(tag)
+                intent_id_dict[id]["metadata"][tag["name"]] = datetime.datetime.now().isoformat()
+
     intents = original_dict["intents"]
     for intent in intents:
-        tag_list = []
-        metadata = {}
-        for tag in tags:
-            intent_name = intent["name"]
-            assert isinstance(intent_name,str)
-            if intent_name.find(tag["name"]) > 0:
-                tag_list.append(tag)
-                metadata[tag["name"]] = datetime.datetime.now().isoformat()
-        intent["tags"] = tag_list.copy()
-        intent["metadata"] = metadata.copy()
+        intent["tags"] = intent_id_dict[intent["id"]]["tags"]
+        intent["metadata"] = intent_id_dict[intent["id"]]["metadata"]
+        if "parent_intent_id" in intent.keys():
+            intent["tags"].extend(intent_id_dict[intent["parent_intent_id"]]["tags"])
+            intent["metadata"].update(intent_id_dict[intent["parent_intent_id"]]["metadata"])
 
     original_dict["intents"] = intents
 
