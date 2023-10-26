ea4591b23012e501451c1257172068859565348c
Stephen Broadhurst <stephen@humanfirst.ai>
2023-10-26 11:54:16 +0100

    CSV convert script

---
+++ csv_to_entities.py
@@ -0,0 +1,97 @@
+"""
+python csv_to_entites.py
+
+accept in a UTF8 csv file and a delimiter and converts it to entities
+your csv should be of the format
+no headers
+filename without .csv is entity name
+col0 = key_value
+col1 = onward are synonyms
+for example this is the way Dialogflow exports an entity
+
+
+
+"""
+# *********************************************************************************************************************
+
+# standard imports
+import json
+
+# 3rd party imports
+import pandas
+import click
+
+
+@click.command()
+@click.option('-f', '--filename', type=str, required=True,
+              help='Input File Path')
+@click.option('-d', '--delimiter', type=str, required=False, default=",",
+              help='Delimiter for the csv file')
+@click.option('-l', '--language', type=str, required=False, default="en",
+              help='Language of entities default: en')
+def main(filename: str,
+         delimiter: str,
+         language:str
+         ) -> None:
+    """Main Function"""
+
+    # read the input csv with columns 0,1,2 etc
+    assert filename.endswith(".csv")
+    df = pandas.read_csv(filename, encoding='utf8',header=None, delimiter=delimiter)
+
+    # assert
+    assert df.shape[0] >= 1
+    assert df.shape[1] >= 2
+
+    # values
+    values = []
+
+    # iterate through dataframe
+    for i in range(df.shape[0]):
+
+        # work out how many synonyms we have (columns 3 onward until NaN)
+        synonyms = []
+        for j in range(2,df.shape[1],1):
+            # exit when come to first NaN value
+            if pandas.isna(df.loc[i,j]):
+                break
+            synonym = {
+                "value": df.loc[i,j]
+            }
+            synonyms.append(synonym.copy())
+
+        # add a value to the entity with those synonyms (colunn 1)
+        value = {
+            "id":f'entity-value-{df.loc[i,1]}',
+            "key_value": df.loc[i,1],
+            "language": language,
+            "synonyms": synonyms
+        }
+        values.append(value.copy())
+
+    # create the entity add the values
+    entity_name = filename.split("/")[-1].replace(".csv","")
+    entity = {
+        "id": f'entity-{entity_name}',
+        "name": entity_name,
+        "values": values
+    }
+
+    # add all entities to workspace as a list
+    workspace = {
+        "entities": [entity]
+    }
+
+    # write to json
+    output_filename = filename.replace(".csv",".json")
+    assert output_filename != filename
+
+    print(df)
+    print(json.dumps(workspace,indent=2))
+
+    with open(output_filename,mode="w",encoding="utf8") as output_file:
+        json.dump(workspace,output_file,indent=2)
+        print(f"Wrote to: {output_filename}")
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
