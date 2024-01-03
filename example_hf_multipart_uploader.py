80edf4b755a63e2eafc2b57e928626423d83b081
Mohammed Fayaz Ansar Jelani <amdfayazz@gmail.com>
2024-01-03 06:51:53 +0000

    update multipart uploader

--- example_hf_multipart_uploader.py
+++ example_hf_multipart_uploader.py
@@ -1,113 +1,54 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# *******************************************************************************************************************120
-#  Code Language:   python
-#  Script:          upload_files_to_hf.py
-#  Imports:         re, json, click, requests, requests_toolbelt
-#  Functions:       main(), upload_multipart(), check_status(), replace(),
-#                   delete_file(), get_conversion_set_id(), get_conversion_source_id()
-#  Description:     Upload files to HumanFirst
-#
+"""
+ Code Language:   python
+ Script:          upload_files_to_hf.py
+ Imports:         re, json, click, requests, requests_toolbelt
+ Functions:       main(), upload_multipart(), check_status(), replace(),
+                  delete_file(), get_conversion_set_id(), get_conversion_source_id()
+ Description:     Upload files to HumanFirst
+"""
 # **********************************************************************************************************************
 
 # standard imports
 import re
 import json
+import os
 
 # third party imports
 import requests
 import requests_toolbelt
 import click
+import humanfirst
 
 
 @click.command()
-@click.option('-u', '--username', type=str, required=True, help='HumanFirst username')
-@click.option('-p', '--password', type=str, required=True, help='HumanFirst password')
+@click.option('-u', '--username', type=str, default='',
+              help='HumanFirst username if not setting HF_USERNAME environment variable')
+@click.option('-p', '--password', type=str, default='',
+              help='HumanFirst password if not setting HF_PASSWORD environment variable')
 @click.option('-n', '--namespace', type=str, required=True, help='HumanFirst namespace')
 @click.option('-h', '--filepath', type=str, required=True, help='file to upload to HumanFirst')
-@click.option('-b', '--upload_name', type=str, required=True, help='name assigned to the uploaded file to HumanFirst')
-@click.option('-s', '--convoset', type=str, required=True, help='Conversation set name / data folder name')
-@click.option('-f', '--force', is_flag=True, default=False, help='Prevents runtime user input')
-def main(username: str, password: str, namespace: str, filepath: str, convoset: str, upload_name: str, force: bool) -> None:
-    """Main function - Pre-step: initially create the data folder and manually upload the file.
-                       Only then a conversation source id gets generated.
+@click.option('-b', '--upload_name', type=str, default="", help='name assigned to the uploaded file to HumanFirst')
+@click.option('-s', '--convoset_name', type=str, required=True, help='Conversation set name / data folder name')
+@click.option('-f', '--force_upload', is_flag=True, default=False, help='Replaces the file name of the ')
+def main(username: str,
+         password: str,
+         namespace: str,
+         filepath: str,
+         convoset_name: str,
+         upload_name: str,
+         force_upload: bool) -> None:
+    """Main function"""
 
-    Parameters
-    ----------
+    hf_api = humanfirst.apis.HFAPI(username=username, password=password)
 
-    username: str
-        HumanFirst username
+    # if conversation set already exists, then it returns the source id of the existing conversation set,
+    # otherwise creates a new one
+    conversation_source_id = hf_api.create_conversation_set(namespace=namespace, convoset_name=convoset_name)
 
-    password: str
-        HumanFirst password
-
-    namespace: str
-        HumanFirst namespace
-
-    filepath: str
-        file to upload to HumanFirst
-
-    convoset: str
-        Conversation set name / data folder name
-
-    upload_name: str
-        name assigned to the uploaded file to HumanFirst
-
-    force: bool
-        Prevents runtime user input
-
-    Returns
-    -------
-
-    None
-    """
-    headers = authorize(username, password)
-    conversation_set_id, conversation_source_id = get_conversion_set_id(headers, namespace, convoset)
-    upload_multipart(headers, namespace, conversation_source_id, filepath, upload_name, force)
-
-
-def upload_multipart(headers: dict, namespace: str, conversation_source_id: str, filepath: str, upload_name: str, force: bool) -> None:
-    """Uploads multipart/form-data to HumanFirst
-
-    Parameters
-    ----------
-
-    headers: dict
-        headers containing content-type and authorization bearer token
-
-    namespace: str
-        HumanFirst namespace
-
-    filepath: str
-        file to upload to HumanFirst
-
-    conversation_source_id: str
-        conversation source id
-
-    upload_name: str
-        name assigned to the uploaded file to HumanFirst
-
-    force: bool
-        Prevents runtime user input
-
-    Returns
-    -------
-
-    None
-    """
-    url = f"https://api.humanfirst.ai/v1alpha1/files/{namespace}/{conversation_source_id}"
-    upload_file = open(filepath, 'rb')
-    payload = requests_toolbelt.multipart.encoder.MultipartEncoder(
-        fields={
-            'format': 'IMPORT_FORMAT_HUMANFIRST_JSON',
-            'file': (upload_name, upload_file, 'application/json')}
-    )
-    # This is the magic bit - you must set the content type to include the boundary information
-    # multipart encoder makes working these out easier
-    headers["Content-Type"] = payload.content_type
-    response = requests.request("POST", url, headers=headers, data=payload)
-    check_status(response, headers, namespace, conversation_source_id, filepath, upload_name, force)
-    upload_file.close()
+    if upload_name == "":
+        upload_name = os.path.basename(filepath).replace(".json","")
+    print(upload_name)
+    hf_api.upload_multipart(namespace, conversation_source_id, filepath, upload_name, force_upload)
 
 
 def check_status(response: requests.models.Response, headers: str, namespace: str,
@@ -150,87 +91,30 @@ def check_status(response: requests.models.Response, headers: str, namespace: st
                 if force is True:
                     replace(headers, namespace, conversation_source_id, filepath, upload_name, force)
                 else:
-                    user_choice = input("1. Replace exisiting file\n2. Upload the file with new name\n3. Quit\nEnter your choice: ")
+                    user_choice = input("""1. Replace exisiting file\n
+                                        2. Upload the file with new name\n3. Quit\nEnter your choice: """)
                     if user_choice == "1":
                         replace(headers, namespace, conversation_source_id, filepath, upload_name, force)
                     elif user_choice == "2":
                         upload_name = input("Enter new name for the file: ")
-                        response = upload_multipart(headers, namespace, conversation_source_id, filepath, upload_name, force)
+                        upload_multipart(headers, namespace, conversation_source_id, filepath, upload_name, force)
                     elif user_choice == "3":
                         quit()
                     else:
-                        raise Exception("Incorrect choice")
+                        raise RuntimeError("Incorrect choice")
             else:
-                raise Exception(f"Got {response.status_code} response: {response.text}")
+                raise RuntimeError(f"Got {response.status_code} response: {response.text}")
         else:
-            raise Exception(f"Got {response.status_code} response: {response.text}")
+            raise RuntimeError(f"Got {response.status_code} response: {response.text}")
     else:
         print(f"{filepath} uploaded successfully to HumanFirst")
 
 
 def replace(headers: str, namespace: str, conversation_source_id: str, filepath: str, upload_name: str, force: bool) -> None:
-    """Replaces the exisitng file with the new file in HumanFirst
+    """Replaces the exisitng file with the new file in HumanFirst"""
 
-    Parameters
-    ----------
-
-    headers: dict
-        headers containing content-type and authorization bearer token
-
-    namespace: str
-        HumanFirst namespace
-
-    filepath: str
-        file to upload to HumanFirst
-
-    conversation_source_id: str
-        conversation source id
-
-    upload_name: str
-        name assigned to the uploaded file to HumanFirst
-
-    force: bool
-        Prevents runtime user input
-
-    Returns
-    -------
-
-    None
-    """
-    delete_file(headers, namespace, conversation_source_id, upload_name)
-    upload_multipart(headers, namespace, conversation_source_id, filepath, upload_name, force)
-
-
-def delete_file(headers: str, namespace: str, conversation_source_id: str, filename: str) -> None:
-    """Deletes data file in HumanFirst
-
-    Parameters
-    ----------
-
-    headers: dict
-        headers containing content-type and authorization bearer tokene
-
-    namespace: str
-        HumanFirst namespace
-
-    conversation_source_id: str
-        conversation source id
-
-    filename: str
-        name of the file to be deleted in HumanFirst
-
-    Returns
-    -------
-
-    None
-    """
-    url = f"https://api.humanfirst.ai/v1alpha1/files/{namespace}/{conversation_source_id}/{filename}"
-    payload = {}
-    response = requests.request("DELETE", url, headers=headers, data=payload)
-    if response.status_code == 200:
-        print(f"Deleted {filename} successfully")
-    else:
-        raise Exception(f"Deleting existing file unsuccessful. \n Got {response.status_code} response.")
+    delete_file(namespace, conversation_source_id, upload_name)
+    upload_multipart(namespace, conversation_source_id, filepath, upload_name, force)
 
 
 def get_conversion_set_id(headers: str, namespace: str, convoset: str) -> tuple:
