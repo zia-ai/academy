2d039607f108ece0939c766be1b084016d88a834
Stephen Broadhurst <stephen@humanfirst.ai>
2024-05-11 11:33:20 +0100

    Adding a template for scripts and doing some data analy

---
+++ template.py
@@ -0,0 +1,21 @@
+"""
+python template.py
+
+"""
+# ******************************************************************************************************************120
+
+# standard imports
+
+# 3rd party imports
+import click
+
+# custom imports
+
+@click.command()
+@click.option('-f', '--filename', type=str, required=True, help='Input File Path')
+def main(filename: str) -> None:
+    """Main Function"""
+    print("Hello World")
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
