2dde3f5f50d966eb2b12ca10d538ddefaff73c4c
Stephen Broadhurst <stephen@humanfirst.ai>
2024-08-08 21:11:04 +0100

    Redrafting

--- epinions_01_data_prep.py
+++ epinions_01_data_prep.py
@@ -42,7 +42,8 @@ import humanfirst
 
 @click.command()
 @click.option('-f', '--filename', type=str, required=False, default='./data/epinions/epinions.txt', help='Input File Path')
-def main(filename: str) -> None: # pylint: disable=unused-argument
+@click.option('-n', '--noskip', is_flag=True, required=False, default=False, help='Input File Path')
+def main(filename: str, noskip: bool) -> None: # pylint: disable=unused-argument
     """Process epinions text"""
     
     
@@ -54,22 +55,31 @@ def main(filename: str) -> None: # pylint: disable=unused-argument
         "stars": numpy.float64,
         "words": str
     }
-    
-    # deleted header #item, user, paid, time, stars, words
-    file_in = open(filename,mode='r',encoding='utf8')
 
-    # parse each line in the format expected - frigged file to remove "/n "with " "
+    # parse each line in the format expected - frigged file to remove "/n " with " " using Textpad
+    # note - unclear why won't parse original file - haven't done byte level check
+    file_in = open(filename,mode='r',encoding="utf8")
+
     short = 0
     errors = 0
     completed = 0
     rows = []
     for i,line in enumerate(file_in):       
+        
+        # skip header "#item, user, paid, time, stars, words"
+        if i == 0:
+            continue
+        
+        # do split
         split_line = line.split(' ')
+        
+        # skip short lines
         if len(split_line) < 5:
             short = short + 1
             print(line)
             continue
-            
+
+        # try and get the data from lines with enouh cols.            
         row = {}
         try:
             
@@ -85,12 +95,14 @@ def main(filename: str) -> None: # pylint: disable=unused-argument
             completed = completed + 1
         except Exception as e:
             print("EXCEPTION")
-            print(i)
-            print(split_line[j])
-            print(e)
+            print(f'row:           {i}')
+            print(f'split_line[j]: {split_line[j]}')
+            print(f'Exception:     {e}')
             errors = errors + 1
+            print("Line:")
             print(line)
-            quit()
+            if noskip:
+                quit()
     file_in.close()
 
     # make dataframe
---
+++ epinions_consolidate_keys_json.py
@@ -0,0 +1,75 @@
+"""
+python epinions_consolidate
+
+"""
+# ******************************************************************************************************************120
+
+# standard imports
+import os
+import json
+
+# 3rd party imports
+import click
+import pandas
+
+SCHEMA_KEYS = ["review_id","item_code","date_drafted","stars_rating", 
+               "amount_paid","review","category","manufacturer","model","title"]
+
+# custom imports
+
+@click.command()
+@click.option('-f', '--filename', type=str, required=False,
+              default='./data/epinions/epinions_data.json', help='Input File')
+@click.option('-s', '--sample', type=int, required=False,
+              default=0, help='Sampling ')
+def main(filename: str, sample: int) -> None: # pylint: disable=unused-argument
+    """Main Function"""
+
+    # list the files
+    file_in = open(filename,mode="r",encoding="utf8")
+    dict_in = json.load(file_in)
+    file_in.close()
+    df = pandas.json_normalize(dict_in["examples"])
+
+    # work out valid invalid
+    gb = df[["context.context_id","metadata.key"]].groupby("context.context_id",as_index=True).count()
+    gb.rename(columns={"metadata.key":"count_metadata_keys"},inplace=True)
+
+    # for each key transform the column
+    for key in SCHEMA_KEYS:
+        df_slice = df.loc[df["metadata.key"] == key,["context.context_id","text"]].copy()
+        df_slice.rename(columns={"text":key},inplace=True)
+        df_slice.set_index("context.context_id",inplace=True)
+        gb = gb.join(df_slice)
+
+    # Summarise which ones we lose
+    print("Distribution of keys")
+    print(gb[["count_metadata_keys","review_id"]].groupby("count_metadata_keys").count())
+
+    # mark the ones we lose as invalid
+    gb["valid"] = gb["count_metadata_keys"] == 10
+    print(gb)
+
+    gb = gb[gb["valid"] == True]
+    print(gb)
+
+    print("Categories")
+    print(gb["category"].unique())
+    print(gb[["category","review_id"]].groupby("category").count().sort_values("review_id",ascending=False))
+
+    print("Manufacturers")
+    print(gb["manufacturer"].unique())
+    print(gb[["manufacturer","review_id"]].groupby("manufacturer").count())
+
+    # output
+    output_filename = filename.replace(".json","_output.csv")
+    assert output_filename != filename
+    gb.to_csv(output_filename,header=True,index=False)
+    print(f'Wrote to: {output_filename}')
+
+def validate_row(row: pandas.Series) -> pandas.Series:
+    return row
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
