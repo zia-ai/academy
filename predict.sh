976ba42724da56d483fdf3ed1d86fed86d1dbfa5
Stephen Broadhurst <stephen@humanfirst.ai>
2024-10-21 16:37:50 +0100

    Correcting delimitering

--- predict.sh
+++ predict.sh
@@ -1,11 +1,6 @@
-curl -L -X POST 'https://api.humanfirst.ai/v1alpha1/nlu/predict/$NAMESPACE/$PLAYBOOK' \
--H 'Authorization: Bearer $BEARER_TOKEN' \
--H 'Content-Type: application/json; charset=utf-8' \
--H 'Accept: application/json' \
--w "@curl-format.txt" 
---data-raw '{
-  "namespace": "$NAMESPACE",
-  "playbook_id": "$PLAYBOOK",
-  "input_utterance": "Hello Charlie how fast can you go?"
-  }
-}'
+curl -L -X POST "https://api.humanfirst.ai/v1alpha1/nlu/predict/$NAMESPACE/$PLAYBOOK" \
+-H "Authorization: Bearer $BEARER_TOKEN" \
+-H "Content-Type: application/json; charset=utf-8" \
+-H "Accept: application/json" \
+--data-raw "{\"namespace\": \"$NAMESPACE\",\"playbook_id\": \"$PLAYBOOK\",\"input_utterance\": \"Hello Charlie how fast can you go?\"}}" \
+| python -m json.tool
